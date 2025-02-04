0849ce99afe595a4f40bd1bfff8c6f37d19ea784
Ludmila <ludmila030301@gmail.com>
2025-02-04 20:01:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
 const searchInput = document.getElementById('search-input');
 const resultsArtist = document.getElementById('result-artist');
-const resultPlaylist = document.getElementById('result-playlists')
+const resultPlaylist = document.getElementById('result-playlists');
 
-function requestApi(serachTerm) {
-    const url = 'http://localhost:300/artists?name_like=${searchTerm}'
-    fecth(url)
-        .them((response) => response.json())
-        .them((result) => displayResults(result))
+function requestApi(searchTerm) {
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`; 
+    fetch(url) 
+        .then((response) => response.json()) 
+        .then((result) => displayResults(result)); 
 }
 
-document.addEventListener('input', function(){
-    const  searchTerm = searchInput.value.toLowCase();
-    if(searchTerm === ''){
+function displayResults(result) {
+    resultPlaylist.classList.add('hidden'); 
+    const artistName = document.getElementById('artist-name');
+    const artistImage = document.getElementById('artist-img');
+
+    result.forEach(Element =>{
+        artistName.innerText = element.name;
+        artistImage.src = element.urlImg;
+
+    });
+
+    resultsArtist.classList.remove('hidden');
+
+}
+
+document.addEventListener('input', function() {
+    const searchTerm = searchInput.value.toLowerCase();
+    if (searchTerm === '') {
         resultPlaylist.classList.add('hidden');
         resultsArtist.classList.remove('hidden');
         return;
     }
 
     requestApi(searchTerm);
-})
+});
