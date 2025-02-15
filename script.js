c2f410a98b9b635244e894e7b8e1006c3ada99ea
Ludmila <ludmila030301@gmail.com>
2025-02-15 18:52:14 -0300

    Update comentarios

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const searchInput = document.getElementById('search-input');
 const resultArtist = document.getElementById("result-artist");
 const resultPlaylist = document.getElementById('result-playlists');
 
+//Comando para ativar Api: json-server --watch api-artists/artists.json --port 3000
 function requestApi(searchTerm) {
     const url = `http://localhost:3000/artists?name_like=${searchTerm}`;
     fetch(url)
